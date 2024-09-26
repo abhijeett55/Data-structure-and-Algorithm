c051105ca3120f149260af3ca79957d4c37f8d42
Abhijeet Biswas <abhijeet.24mca10070@vitbhopal.ac.in>
2024-09-26 20:05:25 +0530

    Power Recursion

---
+++ Cpp/Recursion/Power.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int power(int n , int m) {
+    if(n ==0) {
+        return 1;
+    }
+    else {
+        return power(m, n-1)*m;
+    }
+}
+
+int power1(int n , int m) {
+    if (n ==0 ){
+        return 1;
+    }
+    if(n%2==0)
+    {
+        return (m*m, n/2);
+    }
+    else{
+        return (m*m, (n-1)/2);
+    }
+}
+
+int main() {
+    int r = power(3,r);
+    printf("%d/n",r);
+    return 0;
+}
