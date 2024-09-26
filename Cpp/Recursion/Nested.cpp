8026ef42ccb906dd3e67f7d06ac080025b92446e
Abhijeet Biswas <abhijeet.24mca10070@vitbhopal.ac.in>
2024-09-26 19:20:53 +0530

    Nested Recursion

---
+++ Cpp/Recursion/Nested.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fun (int n) {
+    if (n > 100) {
+        return n -10;
+    }
+    else{
+        return fun (n + 11);
+    }
+}
+
+int main() {
+    int r;
+    r = fun(95);
+    printf("%d\n",r);
+    return 0;
+}
