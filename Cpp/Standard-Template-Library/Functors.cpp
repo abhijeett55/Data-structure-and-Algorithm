51b2c27d4755671a7e834f6dcb0423a7c9aa9564
Abhijeet Biswas <abhijeet.24mca10070@vitbhopal.ac.in>
2024-09-26 13:01:29 +0530

    Functors

--- Cpp/Standard-Template-Library/Functors.cpp
+++ Cpp/Standard-Template-Library/Functors.cpp
@@ -3,6 +3,28 @@
 
 using namespace std;
 
+class increm {
+    private:
+    int num;
+    public : 
+    increm (int n) : num(n) {
+    }
+};
+
+
+int increment (int x) {
+    return (x+1);
+}
+
 int main() {
+
+    int arr[] = { 1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int to_add = 5;
+    transform(arr, arr+ n, arr , increment);
+
+    for(int i = 0; i< n ; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
